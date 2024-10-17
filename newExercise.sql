f55ac9089f684fb611594c59ebb288dba0ac8525
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-17 17:13:09 +0200

    select all teachers that teaches on departments of Maths

--- newExercise.sql
+++ newExercise.sql
@@ -90,3 +90,17 @@ ON `teachers`.`id` = `course_teacher`.`teacher_id`
 ORDER BY `degree_name` ASC, `teachers_name` ASC, `teachers_surname` ASC;
 
 ---------------------------------------------------------------------------------------------------------
+
+-- todo  Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT DISTINCT `teachers`.`id`, `teachers`.`name`, `teachers`.`surname`, `departments`.`name` AS `department_name` 
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = "Dipartimento di Matematica"
+ORDER BY `name` ASC, `surname` ASC;
